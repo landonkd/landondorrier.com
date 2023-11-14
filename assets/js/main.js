c02082b3adfa2222d373b339f86e345d2b86ac36
Landon Dorrier <landondorrier@Landons-MacBook-Pro.local>
2023-11-13 23:05:57 -0500

    feat: move radial relative to mouse position

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Immediately Invoked Function Expression Boilerplate
+ */
+;(function () {
+	
+  const root = document.documentElement;
+ 
+  // Move radial relative to mouse cursor
+  document.addEventListener('mousemove', evt => {
+    let x = evt.clientX / innerWidth;
+    let y = evt.clientY / innerHeight;
+
+    x = x * 50;
+    y = y * 200;
+
+    x = -75 + x;
+    y = -100 + y;
+
+    root.style.setProperty('--mouse-x', x + '%');
+    root.style.setProperty('--mouse-y', y + 'px');
+  });
+
+})();
