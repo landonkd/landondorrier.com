b105f9463aa6db1a0894addd2b2be322bfa8b046
Landon Dorrier <landondorrier@Landons-MacBook-Pro.local>
2023-11-13 23:32:07 -0500

    feat: remove throttle

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@
  
   // Move radial relative to mouse cursor
   document.addEventListener('mousemove', evt => {
+
     let x = evt.clientX / innerWidth;
     let y = evt.clientY / innerHeight;
 
@@ -18,6 +19,7 @@
 
     root.style.setProperty('--mouse-x', x + '%');
     root.style.setProperty('--mouse-y', y + 'px');
+
   });
 
 })();
