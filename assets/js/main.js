9f224e1419d36641e29aba2f374f2028972109ce
Landon Dorrier <landondorrier@Landons-MacBook-Pro.local>
2024-01-28 22:03:14 -0500

    feat: 2024 version of site - initial build; save 2023 site in archive folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,6 @@
  */
 ;(function () {
 	
-  // Vars
-  const root = document.documentElement;
-
-  /**
-   * Mosemove event listener
-   */
-  document.addEventListener('mousemove', evt => {
-    let x = evt.clientX;
-    let y = evt.clientY;
-    
-    radialSticky(x, y);
-  });
-
-  /**
-   * Move the radial to the exact mouse position.
-   */
-  function radialSticky(x, y) {
-    x = 'calc(' + x + 'px - 50%)';
-    y = 'calc(' + y + 'px - 50%)';
-
-    root.style.setProperty('--mouse-x', x);
-    root.style.setProperty('--mouse-y', y);
-  }
+  
 
 })();
